9c703375a7f758e934de828b4a9c4da455022d5b
Chennupati Balu <57926813+CHENNUPATIBALU@users.noreply.github.com>
2020-11-10 15:53:16 +0530

    Add project files.

---
+++ ContactsManager/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace ContactsManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.contactsList = new System.Windows.Forms.ListBox();
+            this.searchTextBox = new System.Windows.Forms.TextBox();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // contactsList
+            // 
+            this.contactsList.FormattingEnabled = true;
+            this.contactsList.ItemHeight = 16;
+            this.contactsList.Location = new System.Drawing.Point(12, 75);
+            this.contactsList.Name = "contactsList";
+            this.contactsList.Size = new System.Drawing.Size(776, 340);
+            this.contactsList.TabIndex = 0;
+            // 
+            // searchTextBox
+            // 
+            this.searchTextBox.Location = new System.Drawing.Point(237, 12);
+            this.searchTextBox.Multiline = true;
+            this.searchTextBox.Name = "searchTextBox";
+            this.searchTextBox.Size = new System.Drawing.Size(346, 31);
+            this.searchTextBox.TabIndex = 1;
+            // 
+            // searchButton
+            // 
+            this.searchButton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.searchButton.Location = new System.Drawing.Point(589, 12);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(84, 31);
+            this.searchButton.TabIndex = 2;
+            this.searchButton.Text = "Search";
+            this.searchButton.UseVisualStyleBackColor = false;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(32, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter text to Search: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(799, 431);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.searchTextBox);
+            this.Controls.Add(this.contactsList);
+            this.Name = "Form1";
+            this.Text = "Contacts Search";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox contactsList;
+        private System.Windows.Forms.TextBox searchTextBox;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ContactsManager/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ContactsManager
+{
+    public partial class Form1 : Form
+    {
+        private readonly string path = @"C:\Users\" + Environment.UserName + @"\Desktop\Contacts.txt";
+        public Form1()
+        {
+            InitializeComponent();
+            writeContacts();
+            addContactsToListBox();
+        }
+        private void writeContacts()
+        {
+            string s ="sunil" + "\n" +"1234567890" + "\n" +
+                      "balu" + "\n" + "9936161519" + "\n" +
+                     "vinay" + "\n"+ "9494937474" + "\n" +
+                     "Nanda" + "\n"+ "9876543210" + "\n"+
+                     "sunil" + "\n" + "1234567890" + "\n";
+            File.WriteAllText(@"C:\Users\" + Environment.UserName + @"\Desktop\Contacts.txt",s);
+        }
+        private void addContactsToListBox()
+        {
+            StreamReader sr = new StreamReader(path);
+            string[] s = new string[100];
+            for (int i = 0; i < s.Length; i++)
+            {
+                s[i] = sr.ReadLine();
+                contactsList.Items.Add(s[i] + "\n");
+            }
+        }
+        private void searchButton_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader(path);
+            bool found = false;
+            int count = 0;
+            string[] s = new string[100];
+            for (int i = 0; i < s.Length; i++)
+            {
+                s[i] = sr.ReadLine();
+            }
+            if(searchTextBox.Text!="")
+            {
+                contactsList.Items.Clear();
+                for (int i = 0; i < s.Length; i = i + 2)
+                {
+                    try
+                    {
+                        if (searchTextBox.Text.ToLower().Equals(s[i]))
+                        {
+                            searchTextBox.BackColor = Color.White;
+                            contactsList.Items.Add(s[i] + " : " + s[i + 1] + "\n");
+                        }
+                        if (s[i].Contains(searchTextBox.Text.ToLower().ToString()))
+                        {
+                            found = true;
+                            count++;
+                        }
+                        else
+                        {
+                            found = false;
+                            continue;
+                        }
+                    }
+                    catch(Exception ex)
+                    {}
+
+                    if (found && count == 1)
+                    {
+                        if(found && !duplicateSearch(s, searchTextBox.Text))
+                        {
+                            contactsList.Items.Clear();
+                            contactsList.ForeColor = Color.Black;
+                            contactsList.Items.Add(s[i] + " : " + s[i + 1] + "\n");
+                            break;  
+                        }
+                        else if(found && duplicateSearch(s, searchTextBox.Text))
+                        {
+                            contactsList.Items.Add(s[i] + " : " + s[i + 1] + "\n");
+                            contactsList.ForeColor = Color.BlueViolet;
+                            MessageBox.Show("Duplicate contact found with name: "+ s[i], "Duplicate",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show(searchTextBox.Text + " not found in contacts");
+                        break;
+                    }
+                }
+            }  
+            else
+            {
+                searchTextBox.BackColor = Color.Red;
+            }
+        }
+        private bool duplicateSearch(string[] s,string st)
+        {
+            int count = 0;
+            for(int i=0;i<s.Length;i=i+2)
+            {
+                if (s[i] == st.ToString())
+                    count++;
+            }
+            if (count > 1)
+                return true;
+            return false;
+        }
+    }
+}
