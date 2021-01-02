bc1d7be90ed2bd02f355f8e0a3759a181fbbb53a
Chennupati Balu <57926813+CHENNUPATIBALU@users.noreply.github.com>
2021-01-02 19:37:28 +0530

    Project updated with new Form(Contacts_Search)

--- ContactsManager/Form1.Designer.cs
+++ ContactsManager/Form1.Designer.cs
@@ -28,73 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.contactsList = new System.Windows.Forms.ListBox();
-            this.searchTextBox = new System.Windows.Forms.TextBox();
-            this.searchButton = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.ContactsSearchFormButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // contactsList
+            // ContactsSearchFormButton
             // 
-            this.contactsList.FormattingEnabled = true;
-            this.contactsList.ItemHeight = 16;
-            this.contactsList.Location = new System.Drawing.Point(12, 75);
-            this.contactsList.Name = "contactsList";
-            this.contactsList.Size = new System.Drawing.Size(776, 340);
-            this.contactsList.TabIndex = 0;
-            // 
-            // searchTextBox
-            // 
-            this.searchTextBox.Location = new System.Drawing.Point(237, 12);
-            this.searchTextBox.Multiline = true;
-            this.searchTextBox.Name = "searchTextBox";
-            this.searchTextBox.Size = new System.Drawing.Size(346, 31);
-            this.searchTextBox.TabIndex = 1;
-            // 
-            // searchButton
-            // 
-            this.searchButton.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.searchButton.Location = new System.Drawing.Point(589, 12);
-            this.searchButton.Name = "searchButton";
-            this.searchButton.Size = new System.Drawing.Size(84, 31);
-            this.searchButton.TabIndex = 2;
-            this.searchButton.Text = "Search";
-            this.searchButton.UseVisualStyleBackColor = false;
-            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(32, 16);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(168, 20);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Enter text to Search: ";
+            this.ContactsSearchFormButton.Location = new System.Drawing.Point(301, 149);
+            this.ContactsSearchFormButton.Name = "ContactsSearchFormButton";
+            this.ContactsSearchFormButton.Size = new System.Drawing.Size(192, 95);
+            this.ContactsSearchFormButton.TabIndex = 0;
+            this.ContactsSearchFormButton.Text = "Contacts Search";
+            this.ContactsSearchFormButton.UseVisualStyleBackColor = true;
+            this.ContactsSearchFormButton.Click += new System.EventHandler(this.ContactsSearchFormButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(799, 431);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.searchButton);
-            this.Controls.Add(this.searchTextBox);
-            this.Controls.Add(this.contactsList);
+            this.Controls.Add(this.ContactsSearchFormButton);
             this.Name = "Form1";
-            this.Text = "Contacts Search";
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.ListBox contactsList;
-        private System.Windows.Forms.TextBox searchTextBox;
-        private System.Windows.Forms.Button searchButton;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ContactsSearchFormButton;
     }
 }
 
